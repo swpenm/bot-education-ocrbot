93fc0fc32df6db7311e748ae87a4449bee0cf822
michhar <michhar@microsoft.com>
2016-10-31 14:31:43 -0700

    added the missing server listen

--- server.js
+++ server.js
@@ -46,6 +46,10 @@ server.get(/.*/, restify.serveStatic({
 	'default': 'index.html'
 }));
 
+server.listen(process.env.port || 3978, function () {
+    console.log('%s listening to %s', server.name, server.url); 
+});
+
 /*
 ****************************************************************/
 
