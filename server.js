4da862cd8e4d80fc037489f5f672fbf588dbac6e
michhar <michhar@microsoft.com>
2016-11-01 07:04:24 -0700

    update to config settings and server calls

--- server.js
+++ server.js
@@ -26,25 +26,20 @@ var bot = new builder.UniversalBot(connector, {
 
 // Get secrets from server environment
 var botConnectorOptions = { 
-    appId: process.env.BOTFRAMEWORK_APPID, 
-    appPassword: process.env.BOTFRAMEWORK_APPSECRET
+    appId: config.CONFIGURATIONS.CHAT_CONNECTOR.APP_ID, 
+    appPassword: config.CONFIGURATIONS.CHAT_CONNECTOR.APP_PASSWORD
 };
 
 // Create bot
 var connector = new builder.ChatConnector(botConnectorOptions);
 var bot = new builder.UniversalBot(connector);
 
-bot.dialog('/', function (session) {
-    
-    //respond with user's message
-    session.send("You said " + session.message.text);
-});
-
 // Setup Restify Server
 var server = restify.createServer();
 
 // Handle Bot Framework messages
-server.post('/api/messages', connector.verifyBotFramework(), connector.listen());
+// server.post('/api/messages', connector.verifyBotFramework(), connector.listen());
+server.post('/api/messages', connector.listen());
 
 // Serve a static web page
 server.get(/.*/, restify.serveStatic({
@@ -52,7 +47,7 @@ server.get(/.*/, restify.serveStatic({
 	'default': 'index.html'
 }));
 
-server.listen(process.env.port || 3978, function () {
+server.listen(process.env.port || process.env.PORT || 3978, function () {
     console.log('%s listening to %s', server.name, server.url); 
 });
 
