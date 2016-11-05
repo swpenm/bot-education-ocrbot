87c75f93091b54c7b7a66e1531eb7f15d1464362
michhar <michhar@microsoft.com>
2016-11-04 23:27:55 -0700

    bot now identifies itself

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ bot.dialog('/', function (session) {
     var extractedUrl = extractUrl(session.message);
 
     if (extractedUrl === "") {
-        session.send("Please give me an image link");
+        session.send("Hello.  I'm a OCRBot.  Please give me an image link and I'll look for words.");
     }
 
     describeImage(extractedUrl, function (error, response, body) {
